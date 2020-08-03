8ce61be7f78f5db3ac5e386a8f3d09bed7f8d886
Elkana Orbach <elkana.orbach@au10tix.com>
2020-08-03 16:11:52 +0300

    Add Project

---
+++ XCSTutorialProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XCSTutorialProject1
+//
+//  Created by Elkana Orbach on 03/08/2020.
+//  Copyright © 2020 Elkana Orbach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ XCSTutorialProject1Tests/XCSTutorialProject1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  XCSTutorialProject1Tests.swift
+//  XCSTutorialProject1Tests
+//
+//  Created by Elkana Orbach on 03/08/2020.
+//  Copyright © 2020 Elkana Orbach. All rights reserved.
+//
+
+import XCTest
+@testable import XCSTutorialProject1
+
+class XCSTutorialProject1Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
